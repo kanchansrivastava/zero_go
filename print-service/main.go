be1dc2c31eb1eb47b67dfae7aca0a125485311f8
Kanchan Srivastava <kanchansrivastava1991@gmail.com>
2024-12-20 15:00:36 +0530

    ADd Initial File

---
+++ print-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("!!!!!!!!! Pimggggg")
+	response := "Print Service is up !!"
+	w.Write([]byte(response)) // Convert string to []byte and write to ResponseWriter
+}
+
+func main() {
+	http.HandleFunc("/health-check", pingHandler)
+	panic(http.ListenAndServe(":80", nil))
+}
